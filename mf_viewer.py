7814dde93dc1c634a738c1410867e55f398455ac
Sandeep Vangipuram <sandeep84@gmail.com>
2018-09-29 20:22:42 +0100

    Add the prototype implementation of basis column

--- mf_viewer.py
+++ mf_viewer.py
@@ -7,12 +7,15 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtSql, QtGui, QtCore
 
+from accountsModel import *
+
 class folioProperties(QWidget):
     folioUpdated = pyqtSignal([str])
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.model = accountsModel()
+
+        self.model = None
         self.index = 0
         self.folionum = None
 
@@ -39,6 +42,12 @@ class folioProperties(QWidget):
         boxLayout = QVBoxLayout(self)
         boxLayout.addWidget(groupbox)
 
+    def setModel(self, model):
+        self.model = model
+        self.updateFields()
+
+    def setIndex(self, idx):
+        self.index = idx
         self.updateFields()
 
     def updateFields(self):
@@ -52,6 +61,92 @@ class folioProperties(QWidget):
         self.nameEdit.setText(self.model.record(self.index).value("folioowner"))
         self.schemeCodeEdit.setText(self.model.record(self.index).value("foliocode"))
         self.fundTypeEdit.setText(self.model.record(self.index).value("foliotype"))
+
+class folioDetails(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.model = None
+        self.index = 0
+
+        self.basisEdit = QLineEdit()
+        self.navEdit = QLineEdit()
+        self.balanceUnitsEdit = QLineEdit()
+        self.currentValueEdit = QLineEdit()
+        self.realisedProfitsEdit = QLineEdit()
+        self.unrealisedProfitsEdit = QLineEdit()
+        self.totalProfitsEdit = QLineEdit()
+        self.xirrEdit = QLineEdit()
+
+        layout = QGridLayout()
+        layout.addWidget(QLabel("Basis"), 0, 0)
+        layout.addWidget(self.basisEdit, 0, 1)
+        layout.addWidget(QLabel("Current NAV"), 1, 0)
+        layout.addWidget(self.navEdit, 1, 1)
+        layout.addWidget(QLabel("Balance Units"), 2, 0)
+        layout.addWidget(self.balanceUnitsEdit, 2, 1)
+        layout.addWidget(QLabel("Current Value"), 3, 0)
+        layout.addWidget(self.currentValueEdit, 3, 1)
+
+        layout.addWidget(QLabel("Realised Profits"), 0, 2)
+        layout.addWidget(self.realisedProfitsEdit, 0, 3)
+        layout.addWidget(QLabel("Unrealised Profits"), 1, 2)
+        layout.addWidget(self.unrealisedProfitsEdit, 1, 3)
+        layout.addWidget(QLabel("Total Profits"), 2, 2)
+        layout.addWidget(self.totalProfitsEdit, 2, 3)
+        layout.addWidget(QLabel("XIRR"), 3, 2)
+        layout.addWidget(self.xirrEdit, 3, 3)
+
+        groupbox = QGroupBox("Folio Details")
+        groupbox.setLayout(layout)
+
+        boxLayout = QVBoxLayout(self)
+        boxLayout.addWidget(groupbox)
+
+    def setModel(self, model):
+        self.model = model
+        self.updateFields()
+
+    def setIndex(self, idx):
+        self.index = idx
+        self.updateFields()
+
+    def updateFields(self):
+        self.basisEdit.setText(str(self.model.record(self.index).value("basis")))
+
+class transactionTable(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.model = accountsModel()
+        transactionsTableView = QTableView()
+        transactionsTableView.setModel(self.model)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(transactionsTableView)
+
+        groupbox = QGroupBox("Transactions")
+        groupbox.setLayout(layout)
+
+        boxLayout = QVBoxLayout(self)
+        boxLayout.addWidget(groupbox)
+
+class transactionUI(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.index = 0
+        self.model = accountsModel()
+
+        self.folioProps = folioProperties()
+        self.folioDetails = folioDetails()
+        self.tranTable = transactionTable()
+
+        self.folioProps.setModel(self.model)
+        self.folioDetails.setModel(self.model)
+
+        grid = QGridLayout(self)
+        grid.addWidget(self.folioProps, 0, 0)
+        grid.addWidget(self.folioDetails, 0, 1)
+        grid.addWidget(self.tranTable, 1, 0, 1, 2)
 
     @pyqtSlot()
     def firstIndex(self):
@@ -73,99 +168,11 @@ class folioProperties(QWidget):
         self.index = self.index - 1
         self.updateFields()
 
-class folioDetails(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        basisEdit = QLineEdit()
-        navEdit = QLineEdit()
-        balanceUnitsEdit = QLineEdit()
-        currentValueEdit = QLineEdit()
-        realisedProfitsEdit = QLineEdit()
-        unrealisedProfitsEdit = QLineEdit()
-        totalProfitsEdit = QLineEdit()
-        xirrEdit = QLineEdit()
-
-        layout = QGridLayout()
-        layout.addWidget(QLabel("Basis"), 0, 0)
-        layout.addWidget(basisEdit, 0, 1)
-        layout.addWidget(QLabel("Current NAV"), 1, 0)
-        layout.addWidget(navEdit, 1, 1)
-        layout.addWidget(QLabel("Balance Units"), 2, 0)
-        layout.addWidget(balanceUnitsEdit, 2, 1)
-        layout.addWidget(QLabel("Current Value"), 3, 0)
-        layout.addWidget(currentValueEdit, 3, 1)
-
-        layout.addWidget(QLabel("Realised Profits"), 0, 2)
-        layout.addWidget(realisedProfitsEdit, 0, 3)
-        layout.addWidget(QLabel("Unrealised Profits"), 1, 2)
-        layout.addWidget(unrealisedProfitsEdit, 1, 3)
-        layout.addWidget(QLabel("Total Profits"), 2, 2)
-        layout.addWidget(totalProfitsEdit, 2, 3)
-        layout.addWidget(QLabel("XIRR"), 3, 2)
-        layout.addWidget(xirrEdit, 3, 3)
-
-        groupbox = QGroupBox("Folio Details")
-        groupbox.setLayout(layout)
-
-        boxLayout = QVBoxLayout(self)
-        boxLayout.addWidget(groupbox)
-
-class accountsModel(QtSql.QSqlTableModel):
-    def __init__(self):
-        super().__init__()
-        
-        self.setTable('portfolio') 
-        self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
-        self.select()
-
-class transactionModel(QtSql.QSqlTableModel):
-    def __init__(self):
-        super().__init__()
-        self.folioFilter = ""
-        
-        self.setTable('transactions') 
-        self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
-        self.select()
-        self.removeColumn(0)
-
-    @pyqtSlot(str)
-    def updateFolioFilter(self, folio):
-        self.folioFilter = "folionum='" + folio + "'"
-        self.setFilter(self.folioFilter)
-
-class transactionTable(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.model = transactionModel()
-        transactionsTableView = QTableView()
-        transactionsTableView.setModel(self.model)
-        
-        layout = QVBoxLayout()
-        layout.addWidget(transactionsTableView)
-
-        groupbox = QGroupBox("Transactions")
-        groupbox.setLayout(layout)
-
-        boxLayout = QVBoxLayout(self)
-        boxLayout.addWidget(groupbox)
-
-class transactionUI(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.folioProps = folioProperties()
-        self.folioDetails = folioDetails()
-        self.tranTable = transactionTable()
-
-        self.folioProps.folioUpdated.connect(self.tranTable.model.updateFolioFilter)
-
-        grid = QGridLayout(self)
-        grid.addWidget(self.folioProps, 0, 0)
-        grid.addWidget(self.folioDetails, 0, 1)
-        grid.addWidget(self.tranTable, 1, 0, 1, 2)
-
+    def updateFields(self):
+        self.folioProps.setIndex(self.index)
+        self.folioProps.updateFields()
+        self.folioDetails.setIndex(self.index)
+        self.folioDetails.updateFields()
 
 class mainWindow(QMainWindow):
     def __init__(self):
@@ -192,21 +199,21 @@ class mainWindow(QMainWindow):
 
         firstItemAct = QAction(QIcon.fromTheme("media-skip-backward"), 'First', self)
         firstItemAct.setStatusTip('First account')
-        firstItemAct.triggered.connect(self.transactionUI.folioProps.firstIndex)
+        firstItemAct.triggered.connect(self.transactionUI.firstIndex)
 
         previousItemAct = QAction(QIcon.fromTheme("media-seek-backward"), 'Previous', self)
         previousItemAct.setShortcut('Ctrl+Left')
         previousItemAct.setStatusTip('Previous account')
-        previousItemAct.triggered.connect(self.transactionUI.folioProps.decrementIndex)
+        previousItemAct.triggered.connect(self.transactionUI.decrementIndex)
 
         nextItemAct = QAction(QIcon.fromTheme("media-seek-forward"), 'Next', self)
         nextItemAct.setShortcut('Ctrl+Right')
         nextItemAct.setStatusTip('Next account')
-        nextItemAct.triggered.connect(self.transactionUI.folioProps.incrementIndex)
+        nextItemAct.triggered.connect(self.transactionUI.incrementIndex)
 
         lastItemAct = QAction(QIcon.fromTheme("media-skip-forward"), 'Last', self)
         lastItemAct.setStatusTip('Last account')
-        lastItemAct.triggered.connect(self.transactionUI.folioProps.lastIndex)
+        lastItemAct.triggered.connect(self.transactionUI.lastIndex)
 
         reportAct = QAction(QIcon.fromTheme("x-office-document"), 'Report', self)
         reportAct.setStatusTip('Generate Report')
