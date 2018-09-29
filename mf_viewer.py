e74ea6f716ff277089310c7e8a0544119485dbcd
Sandeep Vangipuram <sandeep84@gmail.com>
2018-09-29 19:12:52 +0100

    Make the tool buttons go through the folios.

--- mf_viewer.py
+++ mf_viewer.py
@@ -3,12 +3,18 @@ from PyQt5.QtWidgets import (QWidget,
     QGridLayout, QFormLayout, QVBoxLayout, QHBoxLayout,
     QTableView, QGroupBox, QLabel, QLineEdit, QMainWindow, QAction,
     QApplication)
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtSql, QtGui, QtCore
 
 class folioProperties(QWidget):
+    folioUpdated = pyqtSignal([str])
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.model = accountsModel()
+        self.index = 0
+        self.folionum = None
 
         self.folioNumberEdit = QLineEdit()
         self.companyNameEdit = QLineEdit()
@@ -32,6 +38,40 @@ class folioProperties(QWidget):
 
         boxLayout = QVBoxLayout(self)
         boxLayout.addWidget(groupbox)
+
+        self.updateFields()
+
+    def updateFields(self):
+        self.folionum = self.model.record(self.index).value("folionum")
+        self.folioUpdated.emit(self.folionum)
+
+        self.folioNumberEdit.setText(self.model.record(self.index).value("folionum"))
+        self.companyNameEdit.setText(self.model.record(self.index).value("amc"))
+        self.fundNameEdit.setText(self.model.record(self.index).value("folioname"))
+        self.optionTypeEdit.setText(self.model.record(self.index).value("option"))
+        self.nameEdit.setText(self.model.record(self.index).value("folioowner"))
+        self.schemeCodeEdit.setText(self.model.record(self.index).value("foliocode"))
+        self.fundTypeEdit.setText(self.model.record(self.index).value("foliotype"))
+
+    @pyqtSlot()
+    def firstIndex(self):
+        self.index = 0
+        self.updateFields()
+
+    @pyqtSlot()
+    def lastIndex(self):
+        self.index = self.model.rowCount() - 1
+        self.updateFields()
+
+    @pyqtSlot()
+    def incrementIndex(self):
+        self.index = self.index + 1
+        self.updateFields()
+
+    @pyqtSlot()
+    def decrementIndex(self):
+        self.index = self.index - 1
+        self.updateFields()
 
 class folioDetails(QWidget):
     def __init__(self, parent=None):
@@ -71,22 +111,36 @@ class folioDetails(QWidget):
         boxLayout = QVBoxLayout(self)
         boxLayout.addWidget(groupbox)
 
+class accountsModel(QtSql.QSqlTableModel):
+    def __init__(self):
+        super().__init__()
+        
+        self.setTable('portfolio') 
+        self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
+        self.select()
+
 class transactionModel(QtSql.QSqlTableModel):
     def __init__(self):
         super().__init__()
+        self.folioFilter = ""
         
         self.setTable('transactions') 
         self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
         self.select()
         self.removeColumn(0)
 
+    @pyqtSlot(str)
+    def updateFolioFilter(self, folio):
+        self.folioFilter = "folionum='" + folio + "'"
+        self.setFilter(self.folioFilter)
+
 class transactionTable(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        model = transactionModel()
+        self.model = transactionModel()
         transactionsTableView = QTableView()
-        transactionsTableView.setModel(model)
+        transactionsTableView.setModel(self.model)
         
         layout = QVBoxLayout()
         layout.addWidget(transactionsTableView)
@@ -101,10 +155,16 @@ class transactionUI(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.folioProps = folioProperties()
+        self.folioDetails = folioDetails()
+        self.tranTable = transactionTable()
+
+        self.folioProps.folioUpdated.connect(self.tranTable.model.updateFolioFilter)
+
         grid = QGridLayout(self)
-        grid.addWidget(folioProperties(), 0, 0)
-        grid.addWidget(folioDetails(), 0, 1)
-        grid.addWidget(transactionTable(), 1, 0, 1, 2)
+        grid.addWidget(self.folioProps, 0, 0)
+        grid.addWidget(self.folioDetails, 0, 1)
+        grid.addWidget(self.tranTable, 1, 0, 1, 2)
 
 
 class mainWindow(QMainWindow):
@@ -113,7 +173,8 @@ class mainWindow(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        self.setCentralWidget(transactionUI())
+        self.transactionUI = transactionUI()
+        self.setCentralWidget(self.transactionUI)
 
         exitAct = QAction(QIcon.fromTheme("application-exit"), 'Exit', self)
         exitAct.setShortcut('Ctrl+Q')
@@ -131,19 +192,23 @@ class mainWindow(QMainWindow):
 
         firstItemAct = QAction(QIcon.fromTheme("media-skip-backward"), 'First', self)
         firstItemAct.setStatusTip('First account')
+        firstItemAct.triggered.connect(self.transactionUI.folioProps.firstIndex)
 
         previousItemAct = QAction(QIcon.fromTheme("media-seek-backward"), 'Previous', self)
         previousItemAct.setShortcut('Ctrl+Left')
         previousItemAct.setStatusTip('Previous account')
+        previousItemAct.triggered.connect(self.transactionUI.folioProps.decrementIndex)
 
         nextItemAct = QAction(QIcon.fromTheme("media-seek-forward"), 'Next', self)
         nextItemAct.setShortcut('Ctrl+Right')
         nextItemAct.setStatusTip('Next account')
+        nextItemAct.triggered.connect(self.transactionUI.folioProps.incrementIndex)
 
         lastItemAct = QAction(QIcon.fromTheme("media-skip-forward"), 'Last', self)
         lastItemAct.setStatusTip('Last account')
+        lastItemAct.triggered.connect(self.transactionUI.folioProps.lastIndex)
 
-        reportAct = QAction(QIcon.fromTheme("x-office-document"), 'Last', self)
+        reportAct = QAction(QIcon.fromTheme("x-office-document"), 'Report', self)
         reportAct.setStatusTip('Generate Report')
 
         #self.statusBar()
