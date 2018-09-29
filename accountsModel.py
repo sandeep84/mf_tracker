aa873a5fab8986f59b805f4904853867fe012aae
Sandeep Vangipuram <sandeep84@gmail.com>
2018-09-29 20:25:56 +0100

    Make basis column a bit more generic

--- accountsModel.py
+++ accountsModel.py
@@ -4,40 +4,45 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot
 class accountsModel(QtSql.QSqlTableModel):
     def __init__(self):
         super().__init__()
-        
+
         self.setTable('portfolio') 
         self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
         self.select()
 
-    def columnCount(self, parent=QtCore.QModelIndex()):
-        return super(accountsModel, self).columnCount()+1
+        self.sqlColumns = super().columnCount()
+        self.extraColumns = ["basis"]
 
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        return super(accountsModel, self).columnCount()+len(self.extraColumns)
 
     def data(self, index, role=QtCore.Qt.DisplayRole):
-        if role == QtCore.Qt.DisplayRole and index.column()==7:
-            return "Hello"
-        return super(accountsModel, self).data(index, role)
+        if role == QtCore.Qt.DisplayRole and index.column() >= self.sqlColumns:
+            return self.extraColumns[index.column() - self.sqlColumns]
 
+        return super(accountsModel, self).data(index, role)
 
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
         # this is similar to `data`
-        if section==7 and orientation==QtCore.Qt.Horizontal and role==QtCore.Qt.DisplayRole:
-            return 'basis'
-        return super(accountsModel, self).headerData(section, orientation, role)
+        if section >= self.sqlColumns and orientation==QtCore.Qt.Horizontal and role==QtCore.Qt.DisplayRole:
+            print(section)
+            return self.extraColumns[section - self.sqlColumns]
 
+        return super(accountsModel, self).headerData(section, orientation, role)
 
     def flags(self, index):
         # since 2nd column is virtual, it doesn't make sense for it to be Editable
         # other columns can be Editable (default for QSqlTableModel)
-        if index.column()==7:
+        if index.column() >= self.sqlColumns:
             return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled
         return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable
 
     def record(self, index):
         rec = super().record(index)
-        basisField = QtSql.QSqlField("basis")
-        basisField.setValue(76)
-        rec.append(basisField)
+
+        for col in self.extraColumns:
+            newField = QtSql.QSqlField(col)
+            newField.setValue(76)
+            rec.append(newField)
 
         return rec
 
